a6097b01d983a6be39fbd5b4086b38e130864954
Azaz Ahmed Lipu <lipuahmedazaz79@gmail.com>
2023-03-23 20:15:58 +0600

    commit

--- Features/Schemas.py
+++ Features/Schemas.py
@@ -1,66 +1,66 @@
-from pydantic import BaseModel, EmailStr
-from datetime import datetime
-from typing import Optional
+# from pydantic import BaseModel, EmailStr
+# from datetime import datetime
+# from typing import Optional
 
-from pydantic.types import conint
-
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
+# from pydantic.types import conint
 
 
-class PostCreate(PostBase):
-    pass
+# class PostBase(BaseModel):
+#     title: str
+#     content: str
+#     published: bool = True
 
 
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
+# class PostCreate(PostBase):
+#     pass
 
 
-class Post(PostBase):
-    id: int
-    created_at: datetime
-    owner_id: int
-    owner: UserOut
+# class UserOut(BaseModel):
+#     id: int
+#     email: EmailStr
+#     created_at: datetime
 
-    class Config:
-        orm_mode = True
-
-
-class PostOut(BaseModel):
-    Post: Post
-    votes: int
-
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
 
 
-class UserCreate(BaseModel):
-    email: EmailStr
-    password: str
+# class Post(PostBase):
+#     id: int
+#     created_at: datetime
+#     owner_id: int
+#     owner: UserOut
+
+#     class Config:
+#         orm_mode = True
 
 
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
+# class PostOut(BaseModel):
+#     Post: Post
+#     votes: int
+
+#     class Config:
+#         orm_mode = True
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+# class UserCreate(BaseModel):
+#     email: EmailStr
+#     password: str
 
 
-class TokenData(BaseModel):
-    id: Optional[str] = None
+# class UserLogin(BaseModel):
+#     email: EmailStr
+#     password: str
 
 
-class Vote(BaseModel):
-    post_id: int
-    dir: conint(le=1)
+# class Token(BaseModel):
+#     access_token: str
+#     token_type: str
+
+
+# class TokenData(BaseModel):
+#     id: Optional[str] = None
+
+
+# class Vote(BaseModel):
+#     post_id: int
+#     dir: conint(le=1)
